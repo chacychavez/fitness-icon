7ac5b35fdbfedd18831e68809f47fafdc1c56584
chacychavez <cyrus@insynchq.com>
2020-06-28 13:46:25 +0800

    Remove unnecessary styled components in `Event`

--- frontend/src/containers/Event/styled.js
+++ frontend/src/containers/Event/styled.js
@@ -291,51 +291,6 @@ export const EventOnlineRegistrationButton = styled.a`
   margin-top: 20px;
 `;
 
-export const EventDetails = styled.div`
-  margin-top: 60px;
-`;
-
-export const EventDetailsHeader = styled.header`
-  font-family: 'Exo';
-  font-weight: bold;
-  font-size: 28px;
-`;
-
-export const EventDetail = styled.div`
-  font-family: 'Exo';
-  font-weight: 600;
-  font-size: 20px;
-  padding: 18px;
-  border-bottom: 1px solid #c4c4c4;
-
-  &:last-child {
-    border-bottom: none;
-  }
-`;
-
-export const EventDetailHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-`;
-
-export const EventDetailContent = styled.div`
-  font-family: 'Exo';
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 30px;
-  padding: 38px 0 0 30px;
-`;
-
-export const EventDetailContentHeader = styled.header`
-  font-family: 'Exo';
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 32px;
-  margin-bottom: 5px;
-`;
-
 export const AdContainer = styled.div``;
 
 export const AdBanner = styled.div`
